5ee2a7a5b02851d581e333038d2fbb47edf9c67f
legendmic2 <goonoo0731@gmail.com>
2021-12-09 02:17:13 +0900

    [SWEA] 2068. 최대수 구하기
    description: Math.max() 사용

--- src/com/company/algorithmPractice/solved/SWEA/SWEA_2068.java
+++ src/com/company/algorithmPractice/solved/SWEA/SWEA_2068.java
@@ -1,11 +1,10 @@
-package com.company.algorithmPractice;
+package com.company.algorithmPractice.solved.SWEA;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-public class Main {
-
+public class SWEA_2068 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
