653dea2c169cdaa021ff03899c6d289a951dcd82
legendmic2 <goonoo0731@gmail.com>
2021-12-14 15:49:49 +0900

    [SWEA] 2047. 신문 헤드라인
    description:
    - str.toUpperCase()는 str 자체를 바꿔주진 않는다
    - - Locale.ROOT 는 안 들어가도 되는듯

---
+++ src/com/company/algorithmPractice/solved/SWEA/SWEA_2047.java
@@ -0,0 +1,22 @@
+package com.company.algorithmPractice.solved.SWEA;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Locale;
+
+// 2047. 신문 헤드라인
+public class SWEA_2047 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+
+        System.out.println(str.toUpperCase(Locale.ROOT));
+
+
+
+
+
+
+    }
+}
