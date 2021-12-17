182a828359c4ca88da3a9fa3937b621242ae8fbc
legendmic2 <goonoo0731@gmail.com>
2021-12-17 17:46:40 +0900

    [SWEA] 2027. 대각선 출력하기
    description:
    - StringBuilder로 하나씩 추가하는 방식으로 구현
    - - print 매번 하는것보단 효율적
    - 2072번 문제의 파일명을 2027로 잘못 만들어서 해당 코드명 수정

--- src/com/company/algorithmPractice/solved/SWEA/SWEA_2027.java
+++ src/com/company/algorithmPractice/solved/SWEA/SWEA_2027.java
@@ -1,33 +1,16 @@
 package com.company.algorithmPractice.solved.SWEA;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
+// 2027. 대각선 출력하기
 public class SWEA_2027 {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+        StringBuilder str = new StringBuilder("++++");
+        int strSize = str.length();
 
-        int num;
-        int sum;
-        List<Integer> oddSumList = new ArrayList<>();
-
-        int T = sc.nextInt();
-
-        for(int i=0; i<T; ++i){
-            sum = 0;
-            for(int j=0; j<10; ++j){
-                num = sc.nextInt();
-                if(num % 2 == 1){
-                    sum += num; // 홀수는 더한다
-                }
-            }
-            oddSumList.add(sum);
-        }
-
-        for(int i=0; i<T; ++i){
-            System.out.println("#" + (i+1) + " " + oddSumList.get(i));
+        for(int i=0; i<=strSize; ++i){
+            str.insert(i, '#');
+            System.out.println(str);
+            str.deleteCharAt(i);
         }
 
     }
---
+++ src/com/company/algorithmPractice/solved/SWEA/SWEA_2072.java
@@ -0,0 +1,34 @@
+package com.company.algorithmPractice.solved.SWEA;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+// 2072. 홀수만 더하기
+public class SWEA_2072 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int num;
+        int sum;
+        List<Integer> oddSumList = new ArrayList<>();
+
+        int T = sc.nextInt();
+
+        for(int i=0; i<T; ++i){
+            sum = 0;
+            for(int j=0; j<10; ++j){
+                num = sc.nextInt();
+                if(num % 2 == 1){
+                    sum += num; // 홀수는 더한다
+                }
+            }
+            oddSumList.add(sum);
+        }
+
+        for(int i=0; i<T; ++i){
+            System.out.println("#" + (i+1) + " " + oddSumList.get(i));
+        }
+
+    }
+}
